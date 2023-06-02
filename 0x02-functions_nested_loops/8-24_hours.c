d68dc560065af31722354e48746f38754ccff763
SaidaNazhi <saidanazhi@gmail.com>
2023-06-02 09:11:17 -0700

    task completed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,7 +2,7 @@
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59   
+ * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
@@ -25,4 +25,4 @@ void jack_bauer(void)
 		}
 		a++;
 	}
-}	
+}
