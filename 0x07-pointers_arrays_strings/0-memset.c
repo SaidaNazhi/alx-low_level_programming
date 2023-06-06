12dc0eec6e0aa4b7e10764258434515269a8396a
SaidaNazhi <saidanazhi@gmail.com>
2023-06-06 12:46:25 -0700

    ASK COMPLETED

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: starting address
+ * @b: the desired value
+ * @n: number
+ * Return: S
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++;
+			s[1] = b;
+
+	return (s);
+}
